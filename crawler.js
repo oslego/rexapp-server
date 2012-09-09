b619680bd954d2e320d3a21074a92e455254a666
Razvan Caliman <razvan.caliman@gmail.com>
2012-09-09 21:47:08 +0100

    added alpha bank and piraeus configs

--- crawler.js
+++ crawler.js
@@ -4,37 +4,60 @@ var jsdom = require("jsdom");
 var bankutils = require("./bankutils.js");
 
 var banks = [
+    // {
+    //     id: "cec",
+    //     name: "CEC Bank",
+    //     url: "https://www.cec.ro/curs-valutar.aspx",
+    //     selector: {
+    //         tableRow: '.list_table:first tr',
+    //         currency: 'td:nth-child(1)',
+    //         buy: 'td:nth-child(2)',
+    //         sell: 'td:nth-child(3)'
+    //     }
+    // },
+    // {
+    //     id: "bcr",
+    //     name: "BCR",
+    //     url: "http://www.bcr.ro/ro/curs-valutar",
+    //     selector: {
+    //         tableRow: '.fxrates table tr',
+    //         currency: 'td:nth-child(2)',
+    //         buy: 'td:nth-child(3)',
+    //         sell: 'td:nth-child(4)'
+    //     }        
+    // },
+    // {
+    //     id: "brd",
+    //     name: "BRD",
+    //     url: "http://www.brd.ro/piete-financiare/piata-valutara-si-monetara/curs-de-schimb/",
+    //     selector: {
+    //         tableRow: '#content .description > table tr:nth-child(2) tr',
+    //         currency: 'td:nth-child(1)',
+    //         buy: 'td:nth-child(6)',
+    //         sell: 'td:nth-child(7)'
+    //     }        
+    // },
+    // {
+    //     id: "alpha",
+    //     name: "Alpha Bank",
+    //     url: "https://www.alphabank.ro/ro/rate/rate_si_dobanzi.php",
+    //     selector: {
+    //         // TODO: complain about the poor markup of AlphaBank
+    //         tableRow: 'table[style="border: 1px solid #DDDDDD;"]:first tr[height]',
+    //         currency: 'td:nth-child(1)',
+    //         buy: 'td:nth-child(5)',
+    //         sell: 'td:nth-child(6)'
+    //     }        
+    // },
     {
-        id: "cec",
-        name: "CEC Bank",
-        url: "https://www.cec.ro/curs-valutar.aspx",
+        id: "piraeus",
+        name: "Piraeus Bank",
+        url: "http://www.piraeusbank.ro/Banca/Unelte/Istoric-Curs-Valutar.html",
         selector: {
-            tableRow: '.list_table:first tr',
-            currency: 'td:nth-child(1)',
-            buy: 'td:nth-child(2)',
-            sell: 'td:nth-child(3)'
-        }
-    },
-    {
-        id: "bcr",
-        name: "BCR",
-        url: "http://www.bcr.ro/ro/curs-valutar",
-        selector: {
-            tableRow: '.fxrates table tr',
-            currency: 'td:nth-child(2)',
-            buy: 'td:nth-child(3)',
-            sell: 'td:nth-child(4)'
-        }        
-    },
-    {
-        id: "brd",
-        name: "BRD",
-        url: "http://www.brd.ro/piete-financiare/piata-valutara-si-monetara/curs-de-schimb/",
-        selector: {
-            tableRow: '#content .description > table tr:nth-child(2) tr',
-            currency: 'td:nth-child(1)',
-            buy: 'td:nth-child(6)',
-            sell: 'td:nth-child(7)'
+            tableRow: 'table.trezorerie tr',
+            currency: 'td:nth-child(2) span',
+            buy: 'td:nth-child(3) span',
+            sell: 'td:nth-child(4) span'
         }        
     }
 ]
@@ -67,7 +90,7 @@ function extractData(body, config){
           var curr = getValue($(row), config.selector.currency, BankUtils.getCurrencyCode)
           var buy = getValue($(row), config.selector.buy, BankUtils.getCurrencyValue)
           var sell = getValue($(row), config.selector.sell, BankUtils.getCurrencyValue)
-          
+
           if (!curr || !buy || !sell){
               return
           }
