b4583edd680022ba0968c910fd6ea57d7c15d8b9
Razvan Caliman <razvan.caliman@gmail.com>
2012-10-06 23:49:39 +0100

    cleanup

--- schema.js
+++
@@ -1,9 +0,0 @@
-var pg = require('pg').native
-  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/rexdev'
-  , client
-  , query;
-
-client = new pg.Client(connectionString);
-client.connect();
-query = client.query('CREATE TABLE rates (id varchar(10), currency char(3), buy real, sell real, date timestamp)');
-query.on('end', function() { client.end(); });
