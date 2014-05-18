db01c39c2ad7bf77d55dbb8f7532322f97f082e8
Razvan Caliman <razvan.caliman@gmail.com>
2014-05-18 15:33:50 +0100

    update deps for pg

--- ratestore.js
+++ ratestore.js
@@ -1,4 +1,4 @@
-var pg = require('pg').native,
+var pg = require('pg'),
     connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/rexdev',
     noop = function() {},
     isFunction = function(fn){
@@ -11,14 +11,14 @@ function getData(callback, date){
         client = new pg.Client(connectionString),
         numRows = 0,
         query;
-        
+
     query = client.query('SELECT data FROM rates WHERE updated_on = $1', [updated_on])
-    
+
     query.on('row', function(result){
         numRows++;
         callback.call(null, JSON.parse(result.data))
     })
-    
+
     query.on('end', function(err){
         // if no rows return it means we don't have results for the specified date
         if (numRows == 0){
@@ -29,11 +29,11 @@ function getData(callback, date){
             getData.call(this, callback, updated_on)
         }
     })
-    
+
     client.on('drain', function(){
         client.end()
     })
-    
+
     client.connect(function(err){
         if (err){
             console.error("DB connection fail", err)
@@ -46,7 +46,7 @@ function storeData(data, callback){
         callback = isFunction(callback) ? callback : noop,
         client = new pg.Client(connectionString),
         query;
-        
+
     // updated_on is a UNIQUE field in the DB. It is not necessary to use LIMIT in the query.
     query = client.query('UPDATE rates SET data = $2 WHERE updated_on = $1', [updated_on, data], function(err, result){
         if (result.rowCount === 0){
@@ -54,11 +54,11 @@ function storeData(data, callback){
             query.on("row", callback)
         }
     })
-    
+
     client.on('drain', function(){
         client.end()
     })
-    
+
     client.connect()
 }
 
@@ -66,7 +66,7 @@ function RateStore(){
     var _self = this,
         _expires = 1000 * 60, // cache expiration in milliseconds
         _pool = [] // rate data before aggregating and storing into the db
-        
+
     function _getControl(rate){
         if (!_self.cache || !_self.cache.rates || !_self.cache.rates[rate.currency]){
             return
@@ -78,44 +78,44 @@ function RateStore(){
     }
 
     function _getUpdatedOn(rate, control){
-        
+
         function _equals(a, b){
             return a === b
         }
 
-        return ( _equals(rate.sell, control.sell) && _equals(rate.buy, control.buy) ) 
+        return ( _equals(rate.sell, control.sell) && _equals(rate.buy, control.buy) )
             ? control.updated_on
             : +new Date
     }
-    
+
     function setCache(data){
-        _self.cache = data 
-        
+        _self.cache = data
+
         // flush the cache after a set time
         setTimeout(function(){
             _self.cache = null
         }, _expires)
     }
-    
+
     // init
     getData(function(data){
         if (data){
             setCache(data)
         }
     })
-    
+
     return {
         // cache of DB results
         cache: null,
-        
+
         add: function(rateData){
             _pool.push(rateData)
             return this
         },
-        
+
         get: function(currency, callback){
             var result
-            
+
             this.getAll(function(data){
                 if (!data || !data.rates || !data.rates[currency]){
                     result = null
@@ -128,16 +128,16 @@ function RateStore(){
                 }
                 callback.call(null, result)
             })
-            
+
             return this
         },
-        
+
         getAll: function(callback){
-            
+
             if (typeof callback !== 'function'){
                 return this
             }
-            
+
             // Use cache if available to reduce load on DB
             if (_self.cache){
                 callback.call(null, _self.cache)
@@ -150,17 +150,17 @@ function RateStore(){
                         callback.call(null, data)
                     }
                 })
-            } 
-            
+            }
+
             return this
         },
-        
+
         aggregate: function(){
             var obj = {
                 updated_on: new Date,
                 rates: {}
             }
-            
+
             _pool.forEach(function(rate, index){
                 var control = _getControl(rate)
 
@@ -172,11 +172,11 @@ function RateStore(){
 
                 obj.rates[rate.currency].push(rate)
             })
-            
+
             _self.cache = obj
             _pool = []
             this.persist()
-            
+
             return this
         },
 
